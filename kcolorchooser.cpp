bde5605b7615c999da42281f696b5dfb89f6cc96
Christoph Feck <christoph@maxiom.de>
2010-09-07 23:27:04 +0000

    Allow to set initially selected color
    If no color is specified, it tries to read it from the clipboard,
    and falls back to the old default ("blue") if that fails.
    
    FEATURE: 250504
    FIXED-IN: 4.6.0
    
    svn path=/trunk/KDE/kdegraphics/kcolorchooser/; revision=1172743

--- kcolorchooser.cpp
+++ kcolorchooser.cpp
@@ -28,7 +28,9 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <kaboutdata.h>
 #include <klocale.h>
 
-#include "kcolordialog.h"
+#include <kcolordialog.h>
+#include <kcolormimedata.h>
+#include <QtGui/QClipboard>
 
 static const char description[] =
 	I18N_NOOP("KDE Color Chooser");
@@ -46,20 +48,30 @@ int main(int argc, char *argv[])
 
   KCmdLineOptions options;
   options.add("print", ki18n("Print the selected color to stdout"));
+  options.add("color <color>", ki18n("Set initially selected color"));
   KCmdLineArgs::addCmdLineOptions( options );
 
   KApplication app;
   
   KColorDialog dlg;
 
-  dlg.setColor(Qt::blue); // Just a color
+  QColor color = KColorMimeData::fromMimeData( QApplication::clipboard()->mimeData( QClipboard::Clipboard ));
+  if (!color.isValid()) {
+    color = Qt::blue; // Just a color
+  }
+  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+  if (args->isSet("color")) {
+    QColor c = QColor(args->getOption("color"));
+    if (c.isValid())
+      color = c;
+  }
+  dlg.setColor(color);
 
   app.connect(&dlg, SIGNAL(finished()), SLOT(quit()));
 
   dlg.show();
   app.exec();
 
-  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
   const  QColor c = dlg.color();
   if ( args->isSet("print") && c.isValid() ) {
       std::cout << c.name().toUtf8().constData() << std::endl;
